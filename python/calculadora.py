8d4441d6ab563a51f9dcc8805b793fcfda8c6223
Lucas Jaud <lucasjaud19@gmail.com>
2025-05-26 20:23:16 -0300

    atividades na aula de funções

---
+++ python/calculadora.py
@@ -0,0 +1,26 @@
+
+def soma(a, b):
+    return a + b
+
+def mult(a, b):
+    return a * b
+
+def sub(a, b):
+    return a -b
+
+def div(a, b):
+    return a / b
+
+def square_sum(a ,b):
+    return (a + b) ** 2
+
+def execute_op(a, b, func):
+    return func(a, b)
+
+
+print(execute_op(2, 3, soma))
+print(execute_op(2, 3, mult))
+print(execute_op(2, 3, sub))
+print(execute_op(2, 3, div))
+print(execute_op(2, 3, div))
+print(execute_op(2, 3, square_sum))
