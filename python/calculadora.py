1ba13856b848364ac264c929a15f9531b54a01a9
Lucas Jaud <lucasjaud19@gmail.com>
2025-05-26 22:55:04 -0300

    alt

--- python/calculadora.py
+++ python/calculadora.py
@@ -15,12 +15,11 @@ def square_sum(a ,b):
     return (a + b) ** 2
 
 def execute_op(a, b, func):
-    return func(a, b)
+    return f"{func.__name__}: {func(a, b)}"
 
 
 print(execute_op(2, 3, soma))
 print(execute_op(2, 3, mult))
 print(execute_op(2, 3, sub))
-print(execute_op(2, 3, div))
 print(execute_op(2, 3, div))
 print(execute_op(2, 3, square_sum))
